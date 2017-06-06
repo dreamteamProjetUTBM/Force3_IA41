5afc430197351f4b0013e39c1b13abb3a28fcf27
Thomas Gredin <t.gredin@gmx.fr>
2017-06-06 21:37:02 +0200

    Code methodes virtuel dans Scene

--- Force3_IA41/Scene.cpp
+++ Force3_IA41/Scene.cpp
@@ -12,12 +12,24 @@ Scene::~Scene()
 
 void Scene::init(Display * _disp)
 {
+	for (auto gameObject : gameObjects)
+	{
+		gameObject->init(_disp);
+	}
 }
 
 void Scene::update(Display * _disp)
 {
+	for (auto gameObject : gameObjects)
+	{
+		gameObject->update(_disp);
+	}
 }
 
 void Scene::draw(Display * _disp)
 {
+	for (auto gameObject : gameObjects)
+	{
+		gameObject->draw(_disp);
+	}
 }
