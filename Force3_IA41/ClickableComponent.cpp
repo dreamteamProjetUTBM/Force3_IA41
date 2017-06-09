64b9d8b0f6bea3214330e2b61e9ed29dd898c757
Thomas Gredin <t.gredin@gmx.fr>
2017-06-09 15:06:29 +0200

    Ajout des méthodes de Base de Component dans ClickableComponent

--- Force3_IA41/ClickableComponent.cpp
+++ Force3_IA41/ClickableComponent.cpp
@@ -9,6 +9,18 @@ ClickableComponent::~ClickableComponent()
 {
 }
 
+void ClickableComponent::init(Display* _disp)
+{
+}
+
+void ClickableComponent::update(Display* _disp, sf::Event& _event)
+{
+}
+
+void ClickableComponent::draw(Display* _disp)
+{
+}
+
 void ClickableComponent::setClickedCallback(std::function<void(SpriteComponent*)> _click)
 {
 	clicked = _click;
--- Force3_IA41/ClickableComponent.h
+++ Force3_IA41/ClickableComponent.h
@@ -19,7 +19,11 @@ private:
 public:
 	ClickableComponent(sf::String _name, SpriteComponent* _sprite);
 	~ClickableComponent();
-	
+
+	void init(Display* _disp) override;
+	void update(Display* _disp, sf::Event& _event) override;
+	void draw(Display* _disp) override;
+
 	/* Permet de changer la fonction de callback */
 	void setClickedCallback(std::function<void(SpriteComponent*)> _click);
 };
