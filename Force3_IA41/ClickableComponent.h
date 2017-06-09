ab3048d16074309a91d7ccc7459871a0a8939bb8
Thomas Gredin <t.gredin@gmx.fr>
2017-06-09 14:58:44 +0200

    Ajout component ClickableComponent

---
+++ Force3_IA41/ClickableComponent.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <functional>
+
+/*
+ * Classe ClickableComponent
+ * 
+ * Gestion des clickSouris sur un SpriteComponent d'un
+ * GameObject.
+ */
+class ClickableComponent
+{
+private:
+	std::function<void()> clicked;
+
+public:
+	
+};
