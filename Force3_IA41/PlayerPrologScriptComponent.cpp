a7d1076c21321e14078fed373535dfe0a722ccf6
Thomas Gredin <t.gredin@gmx.fr>
2017-06-11 22:17:32 +0200

    Ajout de la classe PlayerPrologScriptComponent

---
+++ Force3_IA41/PlayerPrologScriptComponent.cpp
@@ -0,0 +1,23 @@
+﻿#include "PlayerPrologScriptComponent.h"
+
+PlayerPrologScriptComponent::PlayerPrologScriptComponent(sf::String _name, char* arg0) : Component(_name)
+{
+	plHelper = new PrologHelper(arg0);
+}
+
+PlayerPrologScriptComponent::~PlayerPrologScriptComponent()
+{
+	delete plHelper;
+}
+
+void PlayerPrologScriptComponent::init(Display* _disp)
+{
+}
+
+void PlayerPrologScriptComponent::update(Display* _disp, sf::Event& _event)
+{
+}
+
+void PlayerPrologScriptComponent::draw(Display* _disp)
+{
+}
---
+++ Force3_IA41/PlayerPrologScriptComponent.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "Component.h"
+#include "PrologHelper.h"
+
+/*
+ * Classe PlayerPrologScriptComponent
+ * 
+ * Gestion des scripts prolog pour le joueur.
+ */
+class PlayerPrologScriptComponent : public Component
+{
+private:
+	PrologHelper* plHelper;
+
+public:
+	PlayerPrologScriptComponent(sf::String _name, char* arg0);
+	~PlayerPrologScriptComponent();
+
+	void init(Display* _disp) override;
+	void update(Display* _disp, sf::Event& _event) override;
+	void draw(Display* _disp) override;
+};
