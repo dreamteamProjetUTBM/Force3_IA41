2190e6445a2e049cca367dcc6253c95a3a5febd7
Thomas Gredin <t.gredin@gmx.fr>
2017-06-12 20:53:59 +0200

    Textures pour les cases

--- Force3_IA41/Board.cpp
+++ Force3_IA41/Board.cpp
@@ -8,15 +8,15 @@ Board::Board()
 	_pawns_player_1 = 0;
 	_pawns_player_2 = 0;
 
-	_cases[0] = new Case(CASE_0, "ressources/case.png",100,100);
-	_cases[1] = new Case(CASE_1, "ressources/case.png",200,100);
-	_cases[2] = new Case(CASE_2, "ressources/case.png",300,100);
-	_cases[3] = new Case(CASE_3, "ressources/case.png",100,200);
-	_cases[4] = new Case(CASE_4, "ressources/empty.png",200,200);
-	_cases[5] = new Case(CASE_5, "ressources/case.png",300,200);
-	_cases[6] = new Case(CASE_6, "ressources/case.png",100,300);
-	_cases[7] = new Case(CASE_7, "ressources/case.png",200,300);
-	_cases[8] = new Case(CASE_8, "ressources/case.png",300,300);
+	_cases[0] = new Case(CASE_0, "ressources/case.png", 50, 50);
+	_cases[1] = new Case(CASE_1, "ressources/case.png", 178, 50);
+	_cases[2] = new Case(CASE_2, "ressources/case.png", 306, 50);
+	_cases[3] = new Case(CASE_3, "ressources/case.png", 50, 178);
+	_cases[4] = new Case(CASE_4, "ressources/empty.png", 178, 178);
+	_cases[5] = new Case(CASE_5, "ressources/case.png", 306, 178);
+	_cases[6] = new Case(CASE_6, "ressources/case.png", 50, 306);
+	_cases[7] = new Case(CASE_7, "ressources/case.png", 178, 306);
+	_cases[8] = new Case(CASE_8, "ressources/case.png", 306, 306);
 
 	_cases[2]->SetPawn(new Pawn(2,_cases[2]->getPosition().x, _cases[2]->getPosition().y));
 
